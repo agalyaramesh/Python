135921d39a32132df0534b5af6db63ec5cba5e3f
agalyaramesh <60418757+agalyaramesh@users.noreply.github.com>
2020-01-29 14:36:16 +0530

    Update file2.py

--- file2.py
+++ file2.py
@@ -1,3 +1,10 @@
-s=input('enter character:')
-while not(s=='q' or s=='Q'):
-   S=input('enter character:')
+import random
+n=random.choice(range(10))
+for i in range(5):
+    c=int(input('guess a number:'))
+    if c==n:
+        print('win')
+        exit(0)
+    else:
+            print('try again')
+            print('lost')
